2c92809399d8baea2b182da12a608229c7830176
MJ Ronduen <rjhronduen@mymail.mapua.edu.ph>
2024-07-08 21:12:21 +0800

    fixed jk ff

--- Modules/jk.sv
+++ Modules/jk.sv
@@ -2,16 +2,16 @@ module jk(input clk, reset, j, k, output qout);
 
    reg q;
 
-   always @(posedge clk) begin
-	  if ( (j == 1) & (k == 1) ) begin
-		 q <= ~q;
-	  end else if ( (j == 1) & (k == 0) ) begin
-		 q <= 1;
-	  end else if ( ((j == 0) & (k == 1)) | (reset == 1) ) begin
-		 q <= 0;
-	  end else begin
-		 q <= q;
-	  end
+   always @(negedge clk) begin // negedge so that input values update at posedge (no r.c.)
+      if ( (j == 1) & (k == 1) ) begin
+         q <= ~q;
+      end else if ( (j == 1) & (k == 0) ) begin
+         q <= 1;
+      end else if ( ((j == 0) & (k == 1)) | (reset == 1) ) begin
+         q <= 0;
+      end else begin
+         q <= q;
+      end
    end
 
    assign qout = q;
@@ -20,25 +20,33 @@ endmodule
 
 module test;
    reg clk, reset, j, k;
+   reg [1:0] cnt;
    wire	q;
 
 
-   jk jkFF(clk, reset, j, k, q);
+   jk jkFF(clk, reset, cnt[1], cnt[0], q);
 
    initial begin
-	  clk = 0;
-	  reset = 1;
-	  j = 0;
-	  k = 0;
-	  $monitor("%b %b, j=%b k=%b", clk, reset, j, k);
+      cnt = 2'b00;
+      clk = 0;
+      reset = 1;
+      j = cnt[1];
+      k = cnt[0];
+      $monitor("clk=%b reset=%b, j=%b k=%b, q=%b", clk, reset, cnt[1], cnt[0], q);
    end
 
+   always @(posedge clk) begin // automatic input generator from 00 to 11
+      cnt <= cnt + 2'b01;
+   end
+
+
    initial begin
-	  forever #1 clk = ~clk;
+      forever #1 clk = ~clk;
    end
 
    initial fork
-	  #
+      #1 reset = 0;
+	   #16 $finish;
    join
 
 
